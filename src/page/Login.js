6a2f263d0bbfb39bf5329aa2a1aafe576ad57bcc
devmagrfs <devmagrfs@gmail.com>
2022-03-05 14:45:10 +0900

    모달창 기능만 구현해놓음

--- src/page/Login.js
+++ src/page/Login.js
@@ -13,12 +13,13 @@ import LoginPageSlider from '../components/LoginPageSlider';
 function Login(props) {
     const history = useHistory();
     const dispatch = useDispatch();
+    const modalEl = React.useRef();
 
     const [id, setId] = React.useState("");
     const [nickname, setNickname] = React.useState("");
     const [password, setPassword] = React.useState("");
     const [passwordConfirm, setPasswordConfirm] = React.useState("");
-    const [signupModal, setSignupModal] = React.useState(false);
+    const [isOpen, setIsOpen] = React.useState(false);
 
 
     const handleIdInput = (e) => {
@@ -39,13 +40,11 @@ function Login(props) {
 
     const emailCheck = (id) => {
         let _reg = /^((\w|[\-\.])+)@((\w|[\-\.])+)\.([A-Za-z]+){2,3}$/;
-
         return _reg.test(id);
     }
 
     const pwdCheck = (password) => {
         let _reg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
-
         return _reg.test(password);
     }
 
@@ -79,10 +78,6 @@ function Login(props) {
         // )
     }
 
-    const handleSignupModal = () => {
-        setSignupModal(true);
-    }
-
     const handleLogin = () => {
         if (id === '' || password === '') {
             alert('입력하지 않은 칸이 있습니다!');
@@ -98,31 +93,86 @@ function Login(props) {
         // )
     }
 
+    const handleSignupModal = () => {
+        setIsOpen(true);
+    }
+
+    const handleClickOutside = ({ target }) => {
+        if (isOpen && (!modalEl.current || !modalEl.current.contains(target))) {
+            console.log(modalEl.current.contains(target))
+            setIsOpen(false);
+        }
+    };
+
+
+    React.useEffect(() => {
+        window.addEventListener("click", handleClickOutside);
+        return () => {
+            window.removeEventListener("click", handleClickOutside);
+        };
+    }, [isOpen]);
 
 
     return (
         <>
-            {
-                signupModal &&
-                <SignupModal>
-                    <div>이곳에 회원가입 모달창이 만들어집니다.
-                        <input placeholder="아이디" />
-                        <input />
-                        <input />
-                    </div>
-                </SignupModal>
+            {isOpen &&
+                <SignupModalContainer>
+                    <SignupModal ref={modalEl}>
+                        <div style={{ display: "flex", margin: "0 auto", flexDirection: "column", alignContent: "center", alignItems: "center", justifyContent: "center", width: "100%" }}>
+                            <Input is_width="60%" is_height="30px" placeholder="아이디" _onChange={handleIdInput} />
+                            <Input is_width="60%" is_height="30px" placeholder="비밀번호" _onChange={handlePasswordInput} />
+                            <Input is_width="60%" is_height="30px" placeholder="비밀번호 확인" _onChange={handlePasswordConfirmInput} />
+                            <Button is_width="30%" is_height="30px" _onClick={handleSignup}>회원가입</Button>
+                        </div>
+                    </SignupModal>
+                </SignupModalContainer>
             }
             <LoginPageTitle>타이틀 혹은 로고가 위치합니다.</LoginPageTitle>
             <LoginPageContainer>
                 <LoginPageSlider />
                 <LoginPageLoginBox>
                     <Text is_size="26px">로그인</Text>
-                    <Input is_border="none" placeholder="아이디" is_padding="3px" is_margin="4px" is_border_bottom="1px solid black"></Input>
-                    <Input is_border="none" placeholder="비밀번호" is_padding="3px" is_margin="4px" is_border_bottom="1px solid black"></Input>
-                    <Button>로그인</Button>
+                    <div className="input_box"
+                        style={{
+                            width: "70%",
+                            margin: "30px 0",
+                        }}
+                    >
+                        <Input
+                            is_border="none"
+                            placeholder="아이디"
+                            is_padding="3px"
+                            is_margin="10px"
+                            is_border_bottom="1px solid black"
+                        />
+                        <Input
+                            is_border="none"
+                            placeholder="비밀번호"
+                            is_padding="3px"
+                            is_margin="10px"
+                            is_border_bottom="1px solid black"
+                        />
+                    </div>
+
+                    <Button
+                        _onClick={handleLogin}
+                        is_margin="5px 0 30px 0"
+                        is_padding="5px"
+                        is_cursor
+                        is_weight="600"
+                    >
+                        로그인
+                    </Button>
+
                     <div className="signup_to_box" style={{ textAlign: "center" }}>
                         새로 가입하시겠습니까?
-                        <span onClick={handleSignupModal} style={{ cursor: "pointer" }}>회원가입</span>
+                        &nbsp;
+                        <span
+                            onClick={handleSignupModal}
+                            style={{ cursor: "pointer", fontWeight: "600" }}
+                        >
+                            회원가입
+                        </span>
                     </div>
                     <div className="password_search_box" style={{ textAlign: "center" }}>비밀번호 찾기</div>
                 </LoginPageLoginBox>
@@ -139,18 +189,24 @@ const LoginPageTitle = styled.h1`
 const LoginPageContainer = styled.div`
     display: flex;
     width: 1280px;
+    padding: 30px;
     margin: 0 auto;
     flex-wrap: wrap;
     justify-content: space-around;
     align-items: center;
     border: 1px solid red;
+    box-sizing: border-box;
 `
 
 const LoginPageLoginBox = styled.div`
-
+    width: 340px;
+    // height: 100%;
+    padding: 20px;
+    border: 1px solid blue;
+    box-sizing: border-box;
 `
 
-const SignupModal = styled.div`
+const SignupModalContainer = styled.div`
     position: fixed;
     top: 0px;
     left: 0px;
@@ -159,30 +215,30 @@ const SignupModal = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    background: rgba(0, 0, 0, 0.3);
     z-index: 10;
+`
 
-    > div {
-        position: relative;
-        transition: 0.3s;
-        width: 600px;
-        height: 780px;
-        animation: 400ms ease-in-out 0ms 1 normal forwards running modalIn;
-        box-shadow: rgb(0 0 0 / 9%) 0px 2px 12px 0px;
-        background: gray;
-        display: flex;
+const SignupModal = styled.div`
+    position: relative;
+    transition: 0.3s;
+    width: 600px;
+    height: 600px;
+    animation: 400ms ease-in-out 0ms 1 normal forwards running modalIn;
+    box-shadow: rgb(0 0 0 / 9%) 0px 2px 12px 0px;
+    background: white;
+    display: flex;
 
-        @keyframes modalIn {
-            0%{
-                transform: translateY(600px);
-                opacity: 0;
-            }  
-            100%{
-                transform: translateY(0px);
-                opacity: 1;
-            }
+
+    @keyframes modalIn {
+        0%{
+            transform: translateY(600px);
+            opacity: 0;
+        }  
+        100%{
+            transform: translateY(0px);
+            opacity: 1;
         }
-        
-    }
     }
 `
 
