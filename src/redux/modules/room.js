6bcd76fcc14e4b3583a857f0b43c089c27796238
fatchoi3 <94948873+fatchoi3@users.noreply.github.com>
2022-03-21 23:06:30 +0900

    빠른참가추가

--- src/redux/modules/room.js
+++ src/redux/modules/room.js
@@ -176,7 +176,7 @@ const quickStartPlayer = (id) => {
 const quickStartObserver = (id) => {
     return function (dispatch, getState, { history }) {
         console.log("id", id);
-         api.get(`/lobby/fastPlayer/${id}`)
+         api.get(`/lobby/fastObserver/${id}`)
             .then(function (response) {
                 console.log("response", response.data.roomNum);
                 history.push(`/waiting/${response.data.roomNum}`)
