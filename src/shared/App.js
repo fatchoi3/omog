d6415b0dd5c9b19fdb688b34672e918408a0cc80
devmagrfs <devmagrfs@gmail.com>
2022-04-05 17:14:39 +0900

    lazy로 변경

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,13 +3,6 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 
-
-// import Main from "../page/Main";
-// import Login from "../page/Login";
-// import Game from "../page/Game5";
-// import Waiting from '../page/Waiting';
-// import Result from '../page/Result';
-// import GameStart from "../page/GameStart";
 const Login = lazy(() => import('../page/Login'));
 const Main = lazy(() => import('../page/Main'));
 const Waiting = lazy(() => import('../page/Waiting'));
