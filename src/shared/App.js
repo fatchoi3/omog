d6ce3930dd4f58fa4cd9f14cafa3f49b69259398
devmagrfs <devmagrfs@gmail.com>
2022-04-05 17:13:01 +0900

    이미지 사이즈 수정

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 
 
-import Main from "../page/Main";
-import Login from "../page/Login";
+// import Main from "../page/Main";
+// import Login from "../page/Login";
 // import Game from "../page/Game5";
-import Waiting from '../page/Waiting';
-import Result from '../page/Result';
-import GameStart from "../page/GameStart";
+// import Waiting from '../page/Waiting';
+// import Result from '../page/Result';
+// import GameStart from "../page/GameStart";
+const Login = lazy(() => import('../page/Login'));
+const Main = lazy(() => import('../page/Main'));
+const Waiting = lazy(() => import('../page/Waiting'));
+const GameStart = lazy(() => import('../page/GameStart'));
+const Result = lazy(() => import('../page/Result'));
 
 
 function App() {
   return (
     <>
-      <ConnectedRouter history={history}>
-        <Route path="/main" exact component={Main} />
-        <Route path="/" exact component={Login} />
-        <Route path="/waiting/:roomNum" exact component={Waiting} />
-        {/* <Route path="/game/:roomNum" exact component={Game} /> */}
-        <Route path="/game/:roomNum" exact component={GameStart} />
-        <Route path='/game/result/:roomNum' exact component={Result} />
-      </ConnectedRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ConnectedRouter history={history}>
+          <Route path="/main" exact component={Main} />
+          <Route path="/" exact component={Login} />
+          <Route path="/waiting/:roomNum" exact component={Waiting} />
+          {/* <Route path="/game/:roomNum" exact component={Game} /> */}
+          <Route path="/game/:roomNum" exact component={GameStart} />
+          <Route path='/game/result/:roomNum' exact component={Result} />
+        </ConnectedRouter>
+      </Suspense>
     </>
   );
 }
